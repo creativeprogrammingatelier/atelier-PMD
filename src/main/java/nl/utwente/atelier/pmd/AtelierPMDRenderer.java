4f580fce6648843f0678245502f9a2d38609af5e
Arthur Rump <hello@arthurrump.com>
2020-03-18 20:55:55 +0100

    Try create comments, often results in 404

--- src/main/java/nl/utwente/atelier/pmd/AtelierPMDRenderer.java
+++ src/main/java/nl/utwente/atelier/pmd/AtelierPMDRenderer.java
@@ -1,16 +1,25 @@
 package nl.utwente.atelier.pmd;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Writer;
 import java.util.Iterator;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 
 import net.sourceforge.pmd.RuleViolation;
 import net.sourceforge.pmd.renderers.AbstractIncrementingRenderer;
 
 import nl.utwente.atelier.api.Authentication;
+import nl.utwente.atelier.exceptions.CryptoException;
 import nl.utwente.atelier.pmd.server.Configuration;
+import nl.utwente.processing.Processing;
 
 public class AtelierPMDRenderer extends AbstractIncrementingRenderer {
 
@@ -29,13 +38,16 @@ public class AtelierPMDRenderer extends AbstractIncrementingRenderer {
 
     private class NoWriter extends Writer {
         @Override
-        public void write(char[] cbuf, int off, int len) throws IOException { }
+        public void write(char[] cbuf, int off, int len) throws IOException {
+        }
 
         @Override
-        public void flush() throws IOException { }
+        public void flush() throws IOException {
+        }
 
         @Override
-        public void close() throws IOException { }
+        public void close() throws IOException {
+        }
     }
 
     @Override
@@ -53,15 +65,72 @@ public class AtelierPMDRenderer extends AbstractIncrementingRenderer {
     public void renderFileViolations(Iterator<RuleViolation> violations) throws IOException {
         while (violations.hasNext()) {
             var violation = violations.next();
-            System.out.println("Violation: " + violation.getFilename() + ":" + violation.getBeginLine() + ": " + violation.getDescription());
+
+            var json = new JsonObject();
+
+            // Add snippet to the comment
+            json.addProperty("lineStart", Processing.mapLineNumber(violation.getBeginLine()));
+            json.addProperty("charStart", violation.getBeginColumn());
+            json.addProperty("lineEnd", Processing.mapLineNumber(violation.getEndLine()));
+            json.addProperty("charEnd", violation.getEndColumn());
+            // TODO: add snippet body and context
+            json.addProperty("snippetBody", "");
+            json.addProperty("contextBefore", "");
+            json.addProperty("contextAfter", "");
+
+            // Set the default visibility to private
+            json.addProperty("visibilityState", "private");
+
+            // Set the text of the comment
+            json.addProperty("commentBody", violation.getDescription());
+
+            try {
+                var commentReq = new HttpPost(config.getAtelierHost() + "/api/commentThread/file/" + fileID);
+                commentReq.addHeader("Authorization", "Bearer " + auth.getCurrentToken());
+                commentReq.setEntity(new StringEntity(json.toString()));
+
+                var res = client.execute(commentReq);
+                if (res.getStatusLine().getStatusCode() == 200) {
+                    var resJson = JsonParser.parseReader(new InputStreamReader(res.getEntity().getContent()));
+                    var threadID = resJson.getAsJsonObject().get("ID").getAsString();
+                    System.out.println("Made comment " + threadID + " for rule " + violation.getRule().getName());
+                } else {
+                    System.out.println("Request to make comment failed. Got status " + res.getStatusLine().getStatusCode());
+                }
+            } catch (CryptoException | NullPointerException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public void end() throws IOException {
         for (var err : errors) {
-            System.out.println("Error: " + err.getFile() + ": " + err.getMsg());
+            var json = new JsonObject();
+
+            json.addProperty("visibilityState", "private");
+            json.addProperty("commentBody", err.getMsg());
+
+            try {
+                var commentReq = new HttpPost(config.getAtelierHost() + "/api/commentThread/file/" + fileID);
+                commentReq.addHeader("Authorization", "Bearer " + auth.getCurrentToken());
+                commentReq.setEntity(new StringEntity(json.toString()));
+
+                var res = client.execute(commentReq);
+                if (res.getStatusLine().getStatusCode() == 200) {
+                    var resJson = JsonParser.parseReader(new InputStreamReader(res.getEntity().getContent()));
+                    var threadID = resJson.getAsJsonObject().get("ID").getAsString();
+                    System.out.println("Made comment " + threadID + " for an error");
+                } else {
+                    System.out.println("Request to make comment failed. Got status " + res.getStatusLine().getStatusCode());
+                }
+            } catch (CryptoException | NullPointerException e) {
+                e.printStackTrace();
+            }
         }
+
+        System.out.println("Ending renderer for " + fileID);
+
         this.getWriter().close();
     }
 }
